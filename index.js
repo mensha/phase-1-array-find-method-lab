bd3c83a8993797dc9a320a3330a9bdc8f1622148
mensha <msksalston@gmail.com>
2021-05-22 23:00:46 -0400

    commit tree cad52c0a4e16fb67ba71c73a6ee05346e9061b1b

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-
+function superbowlWin(record) {
+    const answer = record.find(r => r.result === "W")
+    return !!answer ? answer.year : undefined
+    // const found = array1.find(element => element > 10);
+}
